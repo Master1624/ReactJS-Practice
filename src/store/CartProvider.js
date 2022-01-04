f58448ccd01189d7b19ea30423aa63a8bc9825f7
Master1624 <jdpa83@gmail.com>
2022-01-04 17:09:21 -0500

    Remove Items from Cart

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -27,10 +27,30 @@ const cartReducer = (state, action) => {
 		} else {
 			updatedItems = state.items.concat(action.item);
 		}
+
 		return {
 			items: updatedItems,
 			totalAmount: updatedTotalAmount,
 		};
+	}
+	if (action.type === "REMOVE") {
+		const existingCartItemIndex = state.items.findIndex(
+			(item) => item.id === action.id,
+		);
+		const existingItem = state.items[existingCartItemIndex];
+		const updatedTotalAmount = state.totalAmount - existingItem.price;
+		let updatedItems;
+		if (existingItem.amount === 1) {
+			updatedItems = state.items.filter((item) => item.id !== action.id);
+		} else {
+			const updatedItem = {...existingItem, amount: existingItem.amount - 1}
+			updatedItems = [...state.items]
+			updatedItems[existingCartItemIndex] = updatedItem
+		}
+		return {
+			items: updatedItems,
+			totalAmount: updatedTotalAmount
+		}
 	}
 	return defaultCartState;
 };
@@ -51,7 +71,7 @@ const CartProvider = (props) => {
 
 	const cartContext = {
 		items: cartState.items,
-		totalAmount: 0,
+		totalAmount: cartState.totalAmount,
 		addItem: addItemToCartHandler,
 		removeItem: removeItemFromCartHandler,
 	};
